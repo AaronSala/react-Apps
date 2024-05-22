4786a4c8eae29ce5405526c071ff7a21dadb7029
Aaron Sala <salaaron2@gmail.com>
2024-05-22 12:32:25 +0300

    added the image slider images without using any third party library

--- bgColors/src/App.jsx
+++ bgColors/src/App.jsx
@@ -3,16 +3,21 @@ import { useState } from "react";
 import "./App.css";
 import StarRating from "./components/start-rating";
 import RandomColor from "./components/Index";
-
+import ImageSlider from "./components/imageSldier/Index";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
-      <RandomColor />
-      
-      <StarRating  noOfStars={10} />
+       {/*<RandomColor />*/}
+
+      {/*<StarRating noOfStars={10} />*/}
+      <ImageSlider
+        url={"https://picsum.photos/v2/list"}
+        page={"1"}
+        limit={"10"}
+      />
     </>
   );
 }
